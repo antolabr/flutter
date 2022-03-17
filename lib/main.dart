c17c5976024b3ec9fe9b4421b0f7f502b33f8aed
antolabr <antoine@labriet.com>
2022-03-17 16:03:46 +0100

    ajout d'une modale

--- lib/main.dart
+++ lib/main.dart
@@ -33,14 +33,35 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: GestureDetector(
-          onTap: ,
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      )
-    );
+        body: Center(
+          child: GestureDetector(
+            onTap: () {
+              _showDialog(context);
+            },
+            child: const Text("bijour le monde"),
+          ),
+        ));
   }
+}
+
+void _showDialog(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text("le TITRE"),
+        content: const Text("Test"),
+        actions: <Widget>[
+          TextButton(
+            child: const Text("Fermer"),
+            onPressed: () {
+              Navigator.of(context).pop();
+            })
+        ]
+      );
+    }
+  );
 }
